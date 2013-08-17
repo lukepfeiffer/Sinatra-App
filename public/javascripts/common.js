a75cbec52bbdf229dda05d864bf5ce7ccf250517
Fredrick <fredrick@poop.(none)>
2013-08-17 10:58:29 -0400

    style javascript page

---
+++ public/javascripts/common.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#show').click(function(){
+		$('h1').show(400);
+ });	
+$('#hide').click(function(){
+		$('h1').hide(400);
+ });
+$('#toggle').click(function(){
+		$('h4').toggle('slow');
+ });
+});
--- sinatra_app.rb
+++ sinatra_app.rb
@@ -3,18 +3,18 @@
    erb :home
  end
  
- require 'sinatra'
  get '/hi' do
    erb :hi
  end
 
- require 'sinatra'
  get '/hello' do
    erb :hello
  end
 
- require 'sinatra'
  get '/about' do
    erb :about
  end
 
+ get '/javascript' do
+	 erb :javascript
+ end
